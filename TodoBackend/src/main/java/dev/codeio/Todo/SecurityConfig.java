d8e405b1019120a85cf810d2392f56a88c8fd785
yeswanthkamaraj <yeswanthkamaraj25@gmail.com>
2026-03-23 10:22:08 +0530

    Added CorsConfigurationSource in SecurityConfig.java

--- TodoBackend/src/main/java/dev/codeio/Todo/SecurityConfig.java
+++ TodoBackend/src/main/java/dev/codeio/Todo/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -28,5 +33,15 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+ public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration =new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELTE"));
+        configuration.setAllowedHeaders(List.of("*"));
 
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+ }
 }
